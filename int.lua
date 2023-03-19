94b1a7fcbf55d92561e7162e4358334586f23830
Masterzz <61717705+not-masterzz@users.noreply.github.com>
2023-03-19 03:16:23 -0400

    Fixed int.

--- int.lua
+++ int.lua
@@ -3,7 +3,7 @@ local files = { 'whitelisted_players.json', 'profiles.json' }
 local engine = loadstring(game:HttpGet("https://raw.githubusercontent.com/Singularity5490/rbimgui-2/main/rbimgui-2.lua"))()
 local window = engine.new({
     text = "Project Joy",
-    size = Vector3.new(500, 400),
+    size = Vector2.new(500, 400),
     color = Color3.fromRGB(25, 25, 25)
 })
 
